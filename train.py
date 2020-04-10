a3d70e80c9468a12d50e28e08e824a1290eb06fb
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2020-04-10 12:59:23 +0800

    Update train.py

--- train.py
+++ train.py
@@ -23,7 +23,7 @@ cfg = get_cfg()
 
 # cfg.MODEL.DEVICE = 'cpu'
 
-cfg.OUTPUT_DIR = './output'
+cfg.OUTPUT_DIR = '../drive/'My Drive'/output'
 cfg.merge_from_file("./detectron2/configs/COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml")
 cfg.DATASETS.TRAIN = ("trainSet",)
 cfg.DATASETS.TEST = ("testSet",)
