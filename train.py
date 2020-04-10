f954ce668c397157d255815270cad032abf439e2
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2020-04-10 13:11:18 +0800

    Update train.py

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ cfg = get_cfg()
 cfg.OUTPUT_DIR = '../drive/'My Drive'/output
 cfg.merge_from_file("./detectron2/configs/COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml")
 cfg.DATASETS.TRAIN = ("trainSet",)
-cfg.DATASETS.TEST = ("testSet",)
+# cfg.DATASETS.TEST = ("testSet",)
 cfg.DATALOADER.NUM_WORKERS = 6
 
 cfg.SOLVER.CHECKPOINT_PERIOD = 100
@@ -36,7 +36,7 @@ cfg.SOLVER.BASE_LR = 1e-3
 
 cfg.MODEL.WEIGHTS = "detectron2://COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x/139173657/model_final_68b088.pkl"
 #cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 5
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 3
 
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 trainer = DefaultTrainer(cfg)
@@ -44,7 +44,7 @@ trainer.resume_or_load(resume=True)
 
 for i in range(10):
     trainer.train()
-    evaluator = COCOEvaluator("testSet", cfg, False, output_dir="./output/")
+    evaluator = COCOEvaluator("testSet", cfg, False, output_dir = cfg.OUTPUT_DIR)
     val_loader = build_detection_test_loader(cfg, "testSet")
     inference_on_dataset(trainer.model, val_loader, evaluator)
     cfg.SOLVER.MAX_ITER += 100
